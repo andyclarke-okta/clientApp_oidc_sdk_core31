75474f2395d5f744904663b95671a2eff89b4c07
Andy Clarke <andy.clarke@okta.com>
2020-07-01 14:29:25 -0400

    Add project files.

---
+++ clientApp_oidc_sdk/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Okta.AspNetCore;
+using okta_aspnetcore_mvc_example.Models;
+
+namespace okta_aspnetcore_mvc_example.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+        //[Authorize]
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your Login Page.";
+
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                var properties = new AuthenticationProperties();
+                //without this, the redirect defaults to entry point of initialization
+                //properties.RedirectUri = "/Home/PostLogOut";
+                return Challenge(properties, OktaDefaults.MvcAuthenticationScheme);
+
+            }
+
+            return RedirectToAction("PostLogin", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            return new SignOutResult(
+                new[]
+                {
+                     OktaDefaults.MvcAuthenticationScheme,
+                     CookieAuthenticationDefaults.AuthenticationScheme,
+                },
+                new AuthenticationProperties { RedirectUri = "/Home/" });
+
+
+        }
+
+        public ActionResult PostLogin()
+        {
+
+            return View();
+        }
+
+        public ActionResult PostLogOut()
+        {
+
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Authorize]
+        public IActionResult Profile()
+        {
+            return View(HttpContext.User.Claims);
+        }
+    }
+}
---
+++ clientApp_oidc_sdk/Startup.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Okta.AspNetCore;
+
+namespace okta_aspnetcore_mvc_example
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            _ = services.AddAuthentication(options =>
+              {
+                  options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                  options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+              })
+           .AddCookie()
+           .AddOktaMvc(new OktaMvcOptions
+           {
+               // Replace these values with your Okta configuration
+               OktaDomain = Configuration.GetValue<string>("Okta:OktaDomain"),
+               ClientId = Configuration.GetValue<string>("Okta:ClientId"),
+               ClientSecret = Configuration.GetValue<string>("Okta:ClientSecret"),
+               //CallbackPath = Configuration.GetValue<string>("okta:CallbackPath"),
+               PostLogoutRedirectUri = Configuration.GetValue<string>("okta:PostLogoutRedirectUri"),
+               AuthorizationServerId = Configuration.GetValue<string>("Okta:AuthorizationServerId"),
+               Scope = new List<string> { "openid", "profile", "email", "offline_access" }
+           });
+
+
+            services.AddControllersWithViews();
+        }
+
+
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
